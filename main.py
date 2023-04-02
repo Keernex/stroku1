6680dd61dedfce48b5e5e7fd1af942f071814b46
Кушніренко Максим <100068913+Keernex@users.noreply.github.com>
2023-04-02 09:45:43 +0000

    done Ex-4

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-q = list(input('Введіть ряд строк через пробіл: ').split())
+#q = list(input('Введіть ряд строк через пробіл: ').split())
+l=input()
+l=l+" "
+q=[]
+r=""
+for i in range(len(l)):
+  if l[i]==" " and l[i-1]!=" ":
+    q.append(r)
+    r=""
+  elif l[i]=="." or l[i]=="," or l[i]=="!" or l[i]=="?":
+    r+=""
+  else:
+    r+=l[i]
+
 w = input()
 e = 0
 for i in q:
